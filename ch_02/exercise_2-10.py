82d5bb0c069407d8a05c96ac3a52d71814810bc8
Dnzl.exe <cempronziel55@gmail.com>
2023-04-06 18:23:31 +0800

    Numbers Exercises

---
+++ ch_02/exercise_2-10.py
@@ -0,0 +1,4 @@
+# Favorite Number
+favorite_number = 0
+message = f"My Favorite Number is {favorite_number}."
+print(message)
---
+++ ch_02/exercise_2-9.py
@@ -0,0 +1,6 @@
+# Number Eight
+print(4 + 4)
+print(14 - 6)
+print(4 * 2)
+print(int(64 / 8))
+print(2 ** 3)
